1c64760b57fc66e393575db4d6961d2336a97a39
AVAZBEK-QAXXOROV <avazbekqaxxorov461@gmail.com>
2024-04-06 17:58:18 +0500

    fineshed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "textPattern":"url('/11homework/images/mainPicture.png)"
+      },
+    },
+  },
+  plugins: [],
+}
+
